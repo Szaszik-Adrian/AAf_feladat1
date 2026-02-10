9430d6033b7829900be3ab4d04d23bc85795f8d6
Szaszik Adrian <szaszik.adrian@gmail.com>
2026-02-10 21:18:25 +0100

    5 ös feladat fixálás

--- AAf_feladat1/Form1.Designer.cs
+++ AAf_feladat1/Form1.Designer.cs
@@ -29,34 +29,113 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(515, 43);
+            this.button1.Location = new System.Drawing.Point(295, 327);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(249, 128);
+            this.button1.Size = new System.Drawing.Size(248, 102);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(253, 398);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(540, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(540, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(540, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(163, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(540, 105);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(163, 22);
+            this.textBox4.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- AAf_feladat1/Form1.cs
+++ AAf_feladat1/Form1.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using MySql.Data.MySqlClient;
+using System.Xml.Linq;
 using static AAf_feladat1.Students;
 
 
@@ -21,34 +22,33 @@ namespace AAf_feladat1
             InitializeComponent();
         }
 
+        private List<Student> students = new List<Student>();
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            var students = LoadStudentsFromCSV("students.csv");
 
         }
-        List<Student> LoadStudentsFromCSV(string path)
+        private List<Student> LoadStudents(string path)
         {
-            List<Student> students = new List<Student>();
-
+            List<Student> list = new List<Student>();
             string[] lines = File.ReadAllLines(path);
 
             for (int i = 1; i < lines.Length; i++)
             {
-                string[] parts = lines[i].Split(',');
+                string[] p = lines[i].Split(',');
 
-                Student s = new Student
+                list.Add(new Student
                 {
-                    Id = int.Parse(parts[0]),
-                    Name = parts[1],
-                    Email = parts[2],
-                    Age = int.Parse(parts[3])
-                };
-
-                students.Add(s);
+                    Id = int.Parse(p[0]),
+                    Name = p[1],
+                    Email = p[2],
+                    Age = int.Parse(p[3])
+                });
             }
 
-            return students;
+            return list;
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -57,10 +57,77 @@ namespace AAf_feladat1
 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                List<Student> students = LoadStudentsFromCSV(ofd.FileName);
-
+                List<Student> students = LoadStudents(ofd.FileName);
+                students = LoadStudents(ofd.FileName);
+                dataGridView1.DataSource = null;
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.DataSource = students;
                 MessageBox.Show($"Beolvasva: {students.Count} diák");
             }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text.Trim(), out int id))
+            {
+                MessageBox.Show("Id csak szám lehet!");
+                return;
+            }
+
+            if (!int.TryParse(textBox4.Text.Trim(), out int age))
+            {
+                MessageBox.Show("Age csak szám lehet!");
+                return;
+            }
+
+            string name = textBox2.Text.Trim();
+            string email = textBox3.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(email))
+            {
+                MessageBox.Show("Name és Email nem lehet üres!");
+                return;
+            }
+
+            // ha már van ilyen ID, ne engedje (később DB-ben is így lesz)
+            foreach (var s in students)
+            {
+                if (s.Id == id)
+                {
+                    MessageBox.Show("Ez az Id már létezik!");
+                    return;
+                }
+            }
+
+            students.Add(new Student
+            {
+                Id = id,
+                Name = name,
+                Email = email,
+                Age = age
+            });
+
+            // Grid frissítés
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = students;
+
+            // mezők ürítése
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+            MessageBox.Show("Diák hozzáadva!");
         }
     }
 }
